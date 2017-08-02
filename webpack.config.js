6615ecab66ebadb854063a75d4d414afb6daf2fe
yucccc <13450003105@163.com>
2017-08-02 12:48:15 +0800

    @1.1.3

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/main.js', // 项目入口
-  // entry: './src/lib/index.js', // 注释掉原有的
+  // entry: './src/main.js', // 项目入口
+  entry: './src/lib/index.js', // 注释掉原有的
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
