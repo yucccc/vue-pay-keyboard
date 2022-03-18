52c6cccbed19234761e65f1dc005eecc03d6cb84
宇cccc <feyucccc@gmail.com>
2022-03-18 09:57:51 +0800

    fix: keyboard num 6 => 5

--- src/lib/vue-pay-keyboard.vue
+++ src/lib/vue-pay-keyboard.vue
@@ -80,7 +80,7 @@ export default {
             val: [],
             keyList: [
                 [1, 2, 3],
-                [4, 6, 6],
+                [4, 5, 6],
                 [7, 8, 9]
             ],
             payStatus: false
